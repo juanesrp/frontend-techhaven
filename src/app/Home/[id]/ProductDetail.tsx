18b576e1bb5fc58f49a8fa0abe52d7b7101045a3
juanesrp <juanesrp@gmail.com>
2024-05-20 18:37:22 -0500

    Arreglos en el Home en el getProducts y en home/[id]

--- src/app/Home/[id]/ProductDetail.tsx
+++ src/app/Home/[id]/ProductDetail.tsx
@@ -1,12 +1,26 @@
+"use client";
 import CardDetail from "@/components/CardDetail/CardDetail";
 import { getProduct } from "@/helpers/product.helper";
 import { productsPreload } from "@/helpers/productsPreload";
 
 import IProducts from "@/interfaces/IProduct";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const ProductDetail = async ({ params }: { params: { id: number } }) => {
-  const product = await getProduct(params.id);
+const ProductDetail = ({ params }: { params: { id: number } }) => {
+  const [product, setProduct] = useState<IProducts>();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const product = await getProduct(params.id);
+        setProduct(product);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
 
   // const product = productsPreload.find((item) => item.id === Number(params.id));
   return (
--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,12 +1,26 @@
+"use client";
 import Products from "@/components/Products/Products";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import { getProductsDB } from "@/helpers/product.helper";
 import { productsPreload } from "@/helpers/productsPreload";
 import IProducts from "@/interfaces/IProduct";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Home = async () => {
-  const products = await getProductsDB();
+const Home = () => {
+  const [products, setProducts] = useState<IProducts[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const products = await getProductsDB();
+        setProducts(products);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
   // const products = productsPreload;
 
   return (
